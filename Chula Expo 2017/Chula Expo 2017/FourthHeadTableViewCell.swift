330a1ba2275bee6f2bde8b0ad6fd416dc6fa6f8a
Ekkalak Leelasornchai <ekkalakfm@gmail.com>
2017-01-15 14:29:00 +0700

    make my profile

---
+++ Chula Expo 2017/Chula Expo 2017/FourthHeadTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  FourthHeadTableViewCell.swift
+//  Chula Expo 2017
+//
+//  Created by Ekkalak Leelasornchai on 1/15/2560 BE.
+//  Copyright © 2560 Chula Computer Engineering Batch#41. All rights reserved.
+//
+
+import UIKit
+
+class FourthHeadTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+   
+
+}

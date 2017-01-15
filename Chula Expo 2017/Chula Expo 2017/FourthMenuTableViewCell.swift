e4ac0d5b05ebeb99316c6690d5cf79b21af8fd81
Ekkalak Leelasornchai <ekkalakfm@gmail.com>
2017-01-15 16:17:50 +0700

    make my profile

--- Chula Expo 2017/Chula Expo 2017/FourthMenuTableViewCell.swift
+++ Chula Expo 2017/Chula Expo 2017/FourthMenuTableViewCell.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class FourthMenuTableViewCell: UITableViewCell {
 
+    var icon: String?
+        {
+        didSet{
+            updateUI()
+        }
+    }
+    
+    var name: String?
+        {
+        didSet{
+            updateUI()
+        }
+    }
+
+    @IBOutlet weak var menuImg: UIImageView!
+    @IBOutlet weak var menuName: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -20,5 +37,21 @@ class FourthMenuTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    func updateUI(){
+        //Reset old data
+        menuName.text = nil
+        menuImg.image = nil
+        
+        //Set new data
+        if let ficon: String = icon
+        {
+            menuImg.image = UIImage(named: ficon)
+        }
+        if let fName: String = name
+        {
+            menuName.text = fName
+        }
+    }
+    
 
 }

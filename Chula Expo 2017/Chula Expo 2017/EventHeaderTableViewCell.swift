0090c2bfa2b99d90f98ed1f1e091651a97dba487
Joe_Pakpoom <pakpoom.thawee@gmail.com>
2017-01-31 18:14:04 +0700

    to update

---
+++ Chula Expo 2017/Chula Expo 2017/EventHeaderTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  EventHeaderTableViewCell.swift
+//  Chula Expo 2017
+//
+//  Created by Pakpoom on 1/31/2560 BE.
+//  Copyright © 2560 Chula Computer Engineering Batch#41. All rights reserved.
+//
+
+import UIKit
+
+class EventHeaderTableViewCell: UITableViewCell {
+
+    @IBOutlet var topicLabel: UILabel!
+    @IBOutlet var placeLabel: UILabel!
+    @IBOutlet var timeTableCollectionView: UICollectionView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

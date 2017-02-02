83e88863d00a2451ffed555c88f2a50ce90e8488
NOT <Panupong.not@hotmail.com>
2017-02-02 14:22:43 +0700

    Add StageEventDetail expandable cell

---
+++ Chula Expo 2017/Chula Expo 2017/StageDetailCell.swift
@@ -0,0 +1,24 @@
+//
+//  StageDetailCell.swift
+//  Chula Expo 2017
+//
+//  Created by NOT on 2/2/2560 BE.
+//  Copyright © 2560 Chula Computer Engineering Batch#41. All rights reserved.
+//
+
+import UIKit
+
+class StageDetailCell: UITableViewCell {
+
+        override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Chula Expo 2017/Chula Expo 2017/TagController.swift
+++ Chula Expo 2017/Chula Expo 2017/TagController.swift
@@ -1,31 +1,31 @@
+////
+////  TagController.swift
+////  Chula Expo 2017
+////
+////  Created by Pakpoom on 1/9/2560 BE.
+////  Copyright © 2560 Chula Computer Engineering Batch#41. All rights reserved.
+////
 //
-//  TagController.swift
-//  Chula Expo 2017
+//import UIKit
 //
-//  Created by Pakpoom on 1/9/2560 BE.
-//  Copyright © 2560 Chula Computer Engineering Batch#41. All rights reserved.
+//class TagController {
 //
-
-import UIKit
-
-class TagController {
-
-    let tagColor: [String: UIColor] = ["ENG": UIColor(red: 0.467, green: 0.188, blue: 0.141, alpha: 1),
-                                       "MED": UIColor(red: 0, green: 0, blue: 0.545, alpha: 1)]
-    
-    func createTagLabel(x: Int, y: Int, tagName: String) -> UILabel {
-        
-        let tagLabel = UILabel(frame: CGRect(x: x, y: y, width: 40, height: 20))
-        tagLabel.text = tagName
-        tagLabel.textColor = UIColor.white
-        tagLabel.font = UIFont.systemFont(ofSize: 11)
-        tagLabel.backgroundColor = tagColor[tagName]
-        tagLabel.textAlignment = NSTextAlignment.center
-        tagLabel.layer.cornerRadius = tagLabel.bounds.height / 2
-        tagLabel.layer.masksToBounds = true
-        
-        return tagLabel
-        
-    }
-
-}
+//    let tagColor: [String: UIColor] = ["ENG": UIColor(red: 0.467, green: 0.188, blue: 0.141, alpha: 1),
+//                                       "MED": UIColor(red: 0, green: 0, blue: 0.545, alpha: 1)]
+//    
+//    func createTagLabel(x: Int, y: Int, tagName: String) -> UILabel {
+//        
+//        let tagLabel = UILabel(frame: CGRect(x: x, y: y, width: 40, height: 20))
+//        tagLabel.text = tagName
+//        tagLabel.textColor = UIColor.white
+//        tagLabel.font = UIFont.systemFont(ofSize: 11)
+//        tagLabel.backgroundColor = tagColor[tagName]
+//        tagLabel.textAlignment = NSTextAlignment.center
+//        tagLabel.layer.cornerRadius = tagLabel.bounds.height / 2
+//        tagLabel.layer.masksToBounds = true
+//        
+//        return tagLabel
+//        
+//    }
+//
+//}

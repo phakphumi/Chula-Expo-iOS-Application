ba89840484fadf2e139e49e9fc33d75d01c6265e
Ekkalak Leelasornchai <ekkalakfm@gmail.com>
2017-02-03 14:13:25 +0700

    for pulling

---
+++ Chula Expo 2017/Chula Expo 2017/SearchViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SearchViewController.swift
+//  Chula Expo 2017
+//
+//  Created by Ekkalak Leelasornchai on 2/3/2560 BE.
+//  Copyright © 2560 Chula Computer Engineering Batch#41. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

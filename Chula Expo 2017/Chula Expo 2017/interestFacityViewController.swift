c4b5f51457ca47fb475d678f1c4d713d66aa1ef4
Ekkalak Leelasornchai <ekkalakfm@gmail.com>
2017-02-08 16:00:56 +0700

    for pulling

--- Chula Expo 2017/Chula Expo 2017/interestFacityViewController.swift
+++ Chula Expo 2017/Chula Expo 2017/interestFacityViewController.swift
@@ -65,13 +65,7 @@ class interestFacityViewController: UIViewController, UICollectionViewDelegate, 
         layout.minimumLineSpacing = 2.5
         collectionView!.collectionViewLayout = layout
         */
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Register cell classes
-        // self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-        
-        // Do any additional setup after loading the view.
+      
         
     }
     

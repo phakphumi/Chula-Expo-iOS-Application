fddda023665d8fff28b5a635741ee2622fbe6478
NOT <Panupong.not@hotmail.com>
2017-02-13 00:04:38 +0700

    Commit

--- Chula Expo 2017/Chula Expo 2017/AppDelegate.swift
+++ Chula Expo 2017/Chula Expo 2017/AppDelegate.swift
@@ -20,12 +20,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        
         FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         
         downloadActivities()
         downloadZone()
         
         let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
         print("...")
         print(urls[urls.count-1] as URL)
         

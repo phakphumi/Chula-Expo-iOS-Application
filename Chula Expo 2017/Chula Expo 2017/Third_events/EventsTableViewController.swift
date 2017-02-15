dece5a83172adb7b155c29a8d4157f77ea0e0a39
NOT <Panupong.not@hotmail.com>
2017-02-15 23:11:03 +0700

    Fix Segue from event controller

--- Chula Expo 2017/Chula Expo 2017/Third_events/EventsTableViewController.swift
+++ Chula Expo 2017/Chula Expo 2017/Third_events/EventsTableViewController.swift
@@ -9,74 +9,52 @@
 import UIKit
 import CoreData
 
-class EventsTableViewController: CoreDataTableViewController
-{
+class EventsTableViewController: CoreDataTableViewController {
     
-//    var selectedActivityId: String?
-//    var selectedName: String?
-//    var selectedStartTime: NSDate?
-//    var selectedEndTime: NSDate?
-//    var selectedLocationDesc: String?
-//    var selectedDesc: String?
-//    var selectedBannerUrl: String?
-//    var selectedIsHighlight: Bool?
-//    var selectedIsFavorite: Bool?
-//    var selectedReservable: Bool?
-//    var selectedIsReserve: Bool?
-//    var selectedToTags: NSSet?
-//    var selectedToFaculty: String?
-//    var selectedToImages: NSSet?
-//    var selectedToVideos: NSSet?
-//    var selectedDateText: String?
-    
-    var facity: String?
-    {
-        didSet
-        {
-            updateUI()
-        }
-    }
-    var managedObjectContext: NSManagedObjectContext?
-    {
-        didSet
-        {
+    var facity: String? {
+        didSet {
             updateUI()
         }
     }
     
-    fileprivate func updateUI()
-    {
-        if let facity = facity{
+    var managedObjectContext: NSManagedObjectContext? {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    fileprivate func updateUI() {
+        
+        if let facity = facity {
             
-        if let context = managedObjectContext, (facity.characters.count) > 0 {
+            if let context = managedObjectContext, (facity.characters.count) > 0 {
             
-            let request = NSFetchRequest<NSFetchRequestResult>(entityName: "ActivityData")
+                let request = NSFetchRequest<NSFetchRequestResult>(entityName: "ActivityData")
             
-            if facity == "Favourites" {
+                if facity == "Favourites" {
                 
-                request.predicate = NSPredicate(format: "toRound.isFavorite == %@", NSNumber(booleanLiteral: true))
-            }
+                    request.predicate = NSPredicate(format: "toRound.isFavorite == %@", NSNumber(booleanLiteral: true))
+                }
+
+                else if facity == "Reservations" {
                 
-            else if facity == "Reservations" {
+                    request.predicate = NSPredicate(format: "toRound.isReserve == %@", NSNumber(booleanLiteral: true))
+                }
                 
-                request.predicate = NSPredicate(format: "toRound.isReserve == %@", NSNumber(booleanLiteral: true))
-            }
-                
-            else {
+                else {
+                    
                 request.predicate = NSPredicate(format: "any faculty contains[c] %@", facity)
-            }
+                }
             
             request.sortDescriptors = [NSSortDescriptor(
                 key: "activityId",
                 ascending: true
-//                selector: #selector(NSDate.compare(_:))
                 )]
             
             fetchedResultsController = NSFetchedResultsController(
                 fetchRequest: request,
                 managedObjectContext: context,
                 sectionNameKeyPath: nil,
-//                sectionNameKeyPath: "toRound.dateSection",
                 cacheName: nil
             )
             }
@@ -88,78 +66,47 @@ class EventsTableViewController: CoreDataTableViewController
         }
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
-    {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         var cell = UITableViewCell()
         
-        if indexPath.row == 0{
+        if indexPath.row == 0 {
             
             cell = tableView.dequeueReusableCell(withIdentifier: "Header", for: indexPath)
             
-            if let headCell = cell as? HeaderTableViewCell{
+            if let headCell = cell as? HeaderTableViewCell {
                 
-                if tableView.numberOfRows(inSection: 0) == 1{
+                if tableView.numberOfRows(inSection: 0) == 1 {
                     
                     headCell.title1 = "EVENT NOT FOUND!"
                     headCell.title2 = "ไม่พบกิจกรรมที่เกี่ยวข้อง"
                     headCell.iconImage = "crossIcon"
-                    
                 } else {
                     
                     headCell.title1 = "RELATED EVENTS"
                     headCell.title2 = "กิจกรรมที่เกี่ยวข้อง"
                     headCell.iconImage = "relatedeventsIcon"
                 }
-                
-                
             }
+        } else {
             
-        }
-        else{
-            
-            let cell = tableView.dequeueReusableCell(withIdentifier: "eventCell", for: indexPath)
+            cell = tableView.dequeueReusableCell(withIdentifier: "eventCell", for: indexPath)
             
             if let fetchData = fetchedResultsController?.object(at: IndexPath(row: indexPath.row - 1, section: 0)) as? ActivityData {
                 
                 var activityId: String?
-//                var desc: String?
-//                var bannerUrl: String?
-//                var isHighlight: Bool?
-//                var isFavorite: Bool?
-//                var reservable: Bool?
-//                var isReserve: Bool?
-//                var toTags: NSSet?
-//                var toFaculty: String?
-//                var toImages: NSSet?
-//                var toVideos: String?
-//                var dateText: String?
                 var name: String?
                 var toRound: NSSet?
                 var thumbnail: String?
-//                var locationDesc: String?
                 
                 fetchData.managedObjectContext?.performAndWait {
-                        // it's easy to forget to do this on the proper queue
-                        
+                    
                         name = fetchData.name
                         toRound = fetchData.toRound
                         thumbnail = fetchData.thumbnailsUrl
-//                        locationDesc = ""
-                        
                         activityId = fetchData.activityId
-//                      desc = fetchData.desc
-//                      bannerUrl = fetchData.bannerUrl
-//                      isHighlight = fetchData.isHighlight
-//                      isFavorite = fetchData.isFavorite
-//                      reservable = fetchData.reservable
-//                      isReserve = fetchData.isReserve
-//                      toTags = fetchData.toTags
-//                      toFaculty = fetchData.faculty
-//                      toImages = fetchData.toImages
-//                      toVideos = fetchData.video
-//                      dateText = fetchData.dateText
-                       
                 }
+                
                 if let eventCell = cell as? EventFeedCell {
                     
                     eventCell.manageObjectContext = managedObjectContext
@@ -170,10 +117,7 @@ class EventsTableViewController: CoreDataTableViewController
                     eventCell.activityId = activityId
                 }
             }
-
         }
-        
-        // Configure the cell...
         
         cell.selectionStyle = .none
         return cell
@@ -192,7 +136,6 @@ class EventsTableViewController: CoreDataTableViewController
         }
         
         return UITableViewAutomaticDimension
-        
     }
 
 
